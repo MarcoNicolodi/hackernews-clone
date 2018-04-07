b68f44c47833b03dc8a0be1cceaf1a9309e3d818
MarcoNicolodi <marcocontatopro@gmail.com>
2018-04-07 17:26:13 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+let links = [
+    {
+        id: 'link-0',
+        url: 'www.howtographql.com',
+        description: 'Fullstack tutorial for GraphQL',
+    },
+    {
+        id: 'link-1',
+        url: 'egghead.io',
+        description: 'Amazing courses!',
+    }
+]
+
+let idCount = links.length;
+
+const resolvers = {
+    Query: {
+        info: () => `This is the api for a Hackernews clone`,
+        feed: () => links,
+        link: (root, args) => { 
+            return links.filter(l => l.id === args.id)[0];
+        }
+    },
+    Mutation: {
+        post: (root, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            };
+            links.push(link);
+            return link;
+        },
+        updateLink: (root, args) => {            
+            let linkIndex = links.findIndex(link => link.id === args.id);
+            let link = links.splice(linkIndex, 1);
+            link = { ...link, ...args };
+            links.push(link);
+            return link;            
+        }
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+});
+
+const options = {
+    port: 3001,
+};
+
+server.start(options, () => console.log(`Running on localhost:${options.port}`));
